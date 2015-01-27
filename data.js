9ab35ebd2c05162a988371ad63ea185a60f86339
Chris McCandlish <cmccand@gmail.com>
2015-01-26 23:51:36 -0500

    updated

--- data.js
+++ data.js
@@ -11,35 +11,35 @@ image: 'http://pubpages.unh.edu/~mha43/html4.01.jpg'
   name: 'CSS',
   description: 'Everything you need to know about CSS.',
   price: '$1000',
-  image: 'http://pubpages.unh.edu/~mha43/html4.01.jpg'
+  image: 'http://neufelddesign.com/wp-content/uploads/2014/09/creating-css-buttons-red-deer-web-design.png'
 },
 
 {
   name: 'Sass',
   description: 'Everything you need to know about Sass.',
   price: '$1000',
-  image: 'http://pubpages.unh.edu/~mha43/html4.01.jpg'
+  image: 'http://geebzor.com/assets/2013/11/logo-235e394c.png'
 },
 
 {
   name: 'Javascript',
   description: 'Everything you need to know about JS.',
   price: '$1000',
-  image: 'http://pubpages.unh.edu/~mha43/html4.01.jpg'
+  image: 'https://www.planet-source-code.com/vb/2010Redesign/images/LangugeHomePages/Javascript.png'
 },
 
 {
   name: 'jQuery',
   description: 'Everything you need to know about jQuery.',
   price: '$1000',
-  image: 'http://pubpages.unh.edu/~mha43/html4.01.jpg'
+  image: 'http://www.geeks.gallery/wp-content/uploads/2015/01/jQurery.gif'
 },
 
 {
   name: 'AJAX',
   description: 'Everything you need to know about AJAX.',
   price: '$1000',
-  image: 'http://pubpages.unh.edu/~mha43/html4.01.jpg'
+  image: 'http://www.webseoanalytics.com/blog/wp-content/uploads/2011/09/ajax_p.jpg'
 }
 
 
--- templates.js
+++ templates.js
@@ -3,7 +3,7 @@ var templates = {};
 templates.product = [
 '<article>',
 '<h2><%= name %></h2>',
-'<img src=<%=imageUrl%>/>',
+'<img src=<%= image %> />',
 '<p><%= description %></p>',
 '<h3><%= price %></h3>',
 '</article>'
